a0e61efa6cda1df9f3d706ef2baa87a75b53bc59
Mimura Shunichiro <mimuramimusyun@gmail.com>
2018-04-03 00:24:50 +0200

    fix conf path, conf.yml -> config.yml

--- src/run.py
+++ src/run.py
@@ -91,7 +91,7 @@ def load_config():
     """
 
     project_root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    conf_path = os.path.join(project_root_dir, 'config', 'conf.yml')
+    conf_path = os.path.join(project_root_dir, 'config', 'config.yml')
     with open(conf_path, 'r') as yml_file:
         configs = yaml.load(yml_file)
     return configs
