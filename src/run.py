8ec8b0f2734ddf1e84d03eaf3f0846a9752827f7
Mimura Shunichiro <mimuramimusyun@gmail.com>
2018-04-02 23:25:21 +0200

    add table schema in sqlalchemy, update init_db

--- src/run.py
+++ src/run.py
@@ -1,15 +1,35 @@
-def init_db():
+from sqlalchemy import create_engine, Column, Text, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Job(Base):
+    __tablename__ = 'job_ads'
+    id = Column(Integer, primary_key=True)
+    uid = Column(Text)
+    title = Column(Text)
+
+
+def init_db(db_uri):
     """
     Drop all existing tables
     Create table for job ads
     """
-    pass
+    engine = create_engine(db_uri)
+    metadata.drop_all(engine)
+    metadata.create_all(engine)
 
 
 def main():
 
+    db_uri = 'postgres://test:testpass@db:5432/heyjobs'
+    # target_url = 'https://www.heyjobs.de/en/jobs-in-berlin'
+
     # initialize db
-    init_db()
+    init_db(db_uri)
 
 
 if __name__ == '__main__':
