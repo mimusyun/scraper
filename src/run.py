104506a888a3dc4f8f9d6b5ee17c16361c1c539e
Mimura Shunichiro <mimuramimusyun@gmail.com>
2018-04-02 23:14:24 +0200

    add init_db func

--- src/run.py
+++ src/run.py
@@ -1,2 +1,16 @@
+def init_db():
+    """
+    Drop all existing tables
+    Create table for job ads
+    """
+    pass
+
+
+def main():
+
+    # initialize db
+    init_db()
+
+
 if __name__ == '__main__':
-    print("Hello scraper!")
+    main()
