523868791d60b777e38bd90ef9218fb8a547ad6b
Mimura Shunichiro <mimuramimusyun@gmail.com>
2018-04-02 23:59:04 +0200

    add utest, scrape_html, wrong url, wrong_arg

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import prepare_utest
 import unittest
-from run import scrape_html
+from run import scrape_html, URLError
 
 
 class TestScrapeHtml(unittest.TestCase):
@@ -12,6 +12,12 @@ class TestScrapeHtml(unittest.TestCase):
     def test_return_something1(self):
         scraped_html = scrape_html("https://www.heyjobs.de/en/jobs-in-berlin")
         self.assertEqual(len(scraped_html) > 0, True)
+
+    def test_invalid_url0(self):
+        self.assertRaises(URLError, scrape_html, "htp://this.is.wrong")
+
+    def test_invalid_arg_type(self):
+        self.assertRaises(TypeError, scrape_html, 12345)
 
 
 if __name__ == '__main__':
